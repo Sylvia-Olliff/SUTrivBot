861bf7bc207dcdce6650943a0716f894c6169d6e
Joe Olliff <jolliff@boomtownroi.com>
2020-02-05 15:24:27 -0500

    TriviaRepo Added comments to Question class json properties

--- SUTrivBot/Models/Question.cs
+++ SUTrivBot/Models/Question.cs
@@ -8,24 +8,46 @@ namespace SUTrivBot.Models
 {
     public class Question
     {
+        /// <summary>
+        /// Text to be presented to the user for this Question.
+        /// "What color is the sky?" for example
+        /// </summary>
         [JsonProperty("questionText", Required = Required.Always)]
         public string QuestionText { get; set; }
 
+        /// <summary>
+        /// Set of strings representing all acceptable answers
+        /// </summary>
         [JsonProperty("answers", Required = Required.Always)]
         public List<string> Answers { get; set; }
 
+        /// <summary>
+        /// If true, then all strings in Answers must be present in the user's response
+        /// </summary>
         [JsonProperty("answerAll", NullValueHandling = NullValueHandling.Ignore)]
         public bool? AnswerAll { get; set; }
 
+        /// <summary>
+        /// Set of strings representing all acceptable answers if the Question has a bonus portion
+        /// </summary>
         [JsonProperty("bonusAnswers", NullValueHandling = NullValueHandling.Ignore)]
         public List<string> BonusAnswers { get; set; }
 
+        /// <summary>
+        /// If true, then all strings in BonusAnswers must be present in the user's response
+        /// </summary>
         [JsonProperty("bonusAll", NullValueHandling = NullValueHandling.Ignore)]
         public bool? BonusAll { get; set; }
 
+        /// <summary>
+        /// Total base points this Question is worth
+        /// </summary>
         [JsonProperty("points", Required = Required.Always)]
         public int Points { get; set; }
 
+        /// <summary>
+        /// Total Bonus points to be added to the base point value if this question has a bonus portion
+        /// </summary>
         [JsonProperty("bonusPoints", NullValueHandling = NullValueHandling.Ignore)]
         public int? BonusPoints { get; set; }
         
